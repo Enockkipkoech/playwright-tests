bb80ba3989df4a6b8a48e46c0feb88fc48a36c28
Enockkipkoech <enockkipkoech19@gmail.com>
2026-05-19 20:37:33 +0300

    Add fixture for counter with test cases

---
+++ tests/fixtureScopeAndIsolation.spec.ts
@@ -0,0 +1,24 @@
+import { test as base, expect } from '@playwright/test';
+
+let counter = 0;
+
+const test = base.extend({
+    counterFixture: [async ({ }, use) => {
+        counter++;
+        await use(counter);
+    },
+    { scope: 'test' }],  // scope can be 'test' (default) or 'worker' - scope:"worker"
+});
+
+test('Test 1', async ({ counterFixture }) => {
+    console.log(`Test 1 - Counter:', ${counterFixture}`);
+
+});
+
+test('Test 2', async ({ counterFixture }) => {
+    console.log(`Test 2 - Counter:', ${counterFixture}`);
+});
+
+test('Test 3', async ({ counterFixture }) => {
+    console.log(`Test 3 - Counter:', ${counterFixture}`);
+});
